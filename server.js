bd428a4d8a691f84e49060decc0b81d167f5572e
asrate <asrate2121@yahoo.com>
2022-11-28 06:51:50 +0300

    first commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const path = require('path');
-const cors = require('cors');
-const connectDB  = require('./config/db');
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const connectDB = require("./config/db");
 
 const app = express();
 //Connect to Database
@@ -11,8 +11,8 @@ connectDB();
 app.use(express.json({ extended: false }));
 
 app.use(cors());
-//Define Routess 
-app.use('/api/departments', require('./routes/api/departments'));
-const PORT = 5000;
+//Define Routess
+app.use("/api/departments", require("./routes/api/departments"));
+const PORT = 5005;
 console.log(process.env.NODE_ENV);
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
