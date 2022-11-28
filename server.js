01c6e2c3f3c6e082a8aeb1cac59a3d441b437414
asrate <asrate2121@yahoo.com>
2022-11-28 06:54:33 +0300

    first commit

--- server.js
+++ server.js
@@ -15,4 +15,11 @@ app.use(cors());
 app.use("/api/departments", require("./routes/api/departments"));
 const PORT = 5005;
 console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static(path.join(__dirname, "erp-client/build")));
+
+  app.get("/*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "erp-client", "build", "index.html"));
+  });
+}
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
